57c6e0226483efedf6a7a8ffef6d6918a0c70a24
rimkus37700 <khoumamekarim@gmail.com>
2025-09-04 17:27:24 +0200

    Fix build: remove duplicate Router, use BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-// src/App.js
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StepGoLanding from "./pages/StepGoLanding";
 import Success from "./pages/Success";
 import Legal from "./pages/Legal";
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<StepGoLanding />} />
         <Route path="/success" element={<Success />} />
         <Route path="/legal" element={<Legal />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
