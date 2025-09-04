e060bc8c3e7fd4f2eadcb6eeae64979b6ad43b22
rimkus37700 <khoumamekarim@gmail.com>
2025-09-04 14:39:18 +0200

    Fix App.js: single Router import

--- src/App.js
+++ src/App.js
@@ -1,17 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import StepGoLanding from "./pages/StepGoLanding";
-import Success from "./pages/Success";
-
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<StepGoLanding />} />
-        <Route path="/success" element={<Success />} />
-      </Routes>
-    </Router>
-  );
-}
+// src/App.js
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StepGoLanding from "./pages/StepGoLanding";
 import Success from "./pages/Success";
